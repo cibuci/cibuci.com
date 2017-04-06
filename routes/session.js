205b27700a79362f0b23959e5b43ee92998f0921
Wayne <evilcome.com@gmail.com>
2017-04-06 23:28:39 +0800

    add some views

---
+++ routes/session.js
@@ -0,0 +1,15 @@
+var router = require('koa-router')();
+
+router.prefix('/session');
+
+router.get('/add', function (ctx, next) {
+
+});
+
+router.get('/remove', function (ctx, next) {
+
+});
+
+router.post('/')
+
+module.exports = router;
